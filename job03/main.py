88aebdd9bb836e65c5d02195f1a60b45b618fe47
jean-emmanuel-gallo <115216537+jean-emmanuel-gallo@users.noreply.github.com>
2023-01-08 18:26:46 +0100

    Add files via upload

---
+++ job03/main.py
@@ -0,0 +1,6 @@
+fruits = ("pomme", "cerise", "orange")
+fruits_str = "melon"
+print('ma liste originelle est : ' + str(fruits))
+print('mon nouveau fruit est ' + str(fruits_str))
+fruits.append(fruits_str)
+print('un nouvel article a été ajouté' + str(fruits))
